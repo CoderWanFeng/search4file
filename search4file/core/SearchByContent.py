929ae036c21bf94a2abf3a4de225caa1e9790ced
CoderWanFeng <1529577833@qq.com>
2022-06-20 23:33:47 +0800

    create⚙work tree

---
+++ search4file/core/SearchByContent.py
@@ -0,0 +1,11 @@
+class SearchByContent():
+    def search_txt_file(self, path, content):
+        """
+        :param path:
+        :param content:
+        :return:
+        """
+        pass
+
+    def search_word_file(self, path, content):
+        pass
