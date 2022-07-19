552335aa03722661be7e77a2ba1490c44c7ebf7d
CoderWanFeng <875143930@qq.com>
2022-07-20 00:46:22 +0800

    change paras

--- search4file/api/search_by_content.py
+++ search4file/api/search_by_content.py
@@ -4,8 +4,8 @@ from search4file.lib.decorator.except_dec import except_dec
 sbc = SearchByContent()
 
 
-# @except_dec()
-def search_by_content(search_path, content):
-    search_result_dict = sbc.search_files(search_path, content)
+@except_dec()
+def search_by_content(search_path, search_content):
+    search_result_dict = sbc.search_files(search_path, search_content)
     print(search_result_dict)
     return search_result_dict
