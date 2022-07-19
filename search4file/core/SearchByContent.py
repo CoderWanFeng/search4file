b2bece91077b9791ecae4470fddc29831d9e4172
程序员晚枫 <1529577833@qq.com>
2022-07-19 13:12:46 +0800

    add output_dict todo

--- search4file/core/SearchByContent.py
+++ search4file/core/SearchByContent.py
@@ -1,6 +1,8 @@
 import glob
 
 import os
+
+from importlib_metadata import files
 from docx import Document
 from pathlib import Path
 
@@ -13,8 +15,27 @@ class SearchByContent():
                 _path = glob.os.path.join(file_path, '*')
                 self.search_files(_path, content)
             else:
+                #todo:输出格式，需要统一
+                # output_dict = {
+                #     'search_path':search_path,
+                #     'content':content,
+                #     #找不到时：
+                #     # 'search_result':None
+                #     'search_result':{
+                #         'files':{
+                #             1:file_path,
+                #             2:file_path
+                #         },
+                #         'word':{
+                #             1:file_path,
+                #             2:file_path
+                #         }
+                #     }
+                # }
                 if file_path.endswith("docx"):
                     self.search_word_file(file_path, content)
+                elif file_path.endswith("xlsx"):
+                    self.search_excel_file
                 else:
                     self.search_txt_file(file_path, content)
 
@@ -42,3 +63,12 @@ class SearchByContent():
             if paragraph.text.find(content) >= 0:
                 print(f'该文件内，包含：【{content}】 | | {search_path}')
 
+    def search_pdf_file(self, search_path, content):
+        pass
+
+    def search_ppt_file(self, search_path, content):
+        pass
+        
+    def search_excel_file(self, search_path, content):
+        pass
+    
